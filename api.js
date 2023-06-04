01e2403cf0e53a18bb5e60cb9ae968cf2993091e
BaoTheTran <ttbao2710@gmail.com>
2023-06-04 14:07:10 +0700

    create project

---
+++ api.js
@@ -0,0 +1,10 @@
+function Api() {
+    this.callApi = function (uri, method, data) {
+      var url = "https://64709e2a3de51400f7249feb.mockapi.io";
+      return axios({
+        url: `${url}/${uri}`,
+        method: method,
+        data: data,
+      });
+    };
+  }
