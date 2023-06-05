678de7564f9217f258f8be7c57e4a14d6757ecc9
BaoTheTran <ttbao2710@gmail.com>
2023-06-05 12:40:43 +0700

    working

--- customer/controllers/mainCustomer.js
+++ customer/controllers/mainCustomer.js
@@ -14,3 +14,15 @@ function fetchProductList(){
 };
 
 fetchProductList();
+
+domId('selectList').onchange = function filterProduct(){
+    var select = domId('selectList').value;
+    var promise = api.callApi("SaiGonPhone","GET",null)
+    promise.then(function(res){
+        const filterProduct = res.data.filter((product)=> product.type ==select);
+        renderProductList(filterProduct);
+    })
+    .catch(function(err){
+        console.log(err);
+    });
+}
